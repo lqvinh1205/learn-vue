c68c79700bd6dfb174b0844f6f7f1a7abc4c24c4
lqvinh1205 <77132358+lqvinh1205@users.noreply.github.com>
2022-07-18 09:25:35 +0700

    .

---
+++ src/components/MainNavbar.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="nav">
+		<div class="logo">
+			<img :src="require('@/assets/Logo.svg')" alt="">
+		</div>
+		<div class="group_menu">
+			<ul>
+				<li>
+					<router-link to="/">
+						Home
+					</router-link>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div class="navbar">
+      <MainNavbar />
+    </div>
+    <div class="main">
+      <div class="sidebar"></div>
+      <div class="content"></div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MainNavbar from "@/components/MainNavbar.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    MainNavbar,
+  },
+};
 </script>
